2da4b78bc3d2e12fc6fcd9b75e5bf9a7cc9a14a3
benjaminrdickson <bdickson8619@gmail.com>
2021-06-18 11:07:53 -0500

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,28 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <button @click="showAlert()">Hello World</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
+/* global Swal */
 export default {
   name: "Home",
   components: {
     HelloWorld,
+  },
+  created: function () {
+    Swal.fire({
+      title: "Question!",
+      text: "Do you want to continue",
+      icon: "error",
+      confirmButtonText: "Yes",
+    });
   },
 };
 </script>
